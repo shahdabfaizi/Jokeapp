66a4d5b5f23eba66356d27ccd0a5e6e4744a2079
shahdabfaizi <shadabfaizi202@gmail.com>
2024-08-11 21:43:38 +0200

    s

--- vite.cofig.js
+++ vite.cofig.js
@@ -1,3 +1,12 @@
-export default {
-    base: "/Jokeapp/",
-};
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: "/Jokeapp/",
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'style/style.css', // Anpassung des Namensschemas
+      },
+    },
+  },
+});
